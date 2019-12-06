fd733a207a7a457b5a22be053010126c34ec8532
Cuthbert Chow <cuthbert.chow@gmail.com>
2019-12-06 16:57:15 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const table = document.querySelector('#resultstable');
 const tooltip = document.querySelector('#tooltip');
 const sentiment = document.querySelector('#sentiment');
 
+const results = document.querySelector('#results');
+const dashboard = document.querySelector('#dashboard');
+
+let fetchstatus = false;
+
 sentiment.addEventListener('mouseover', e => {
     rect = sentiment.getBoundingClientRect()
     tooltip.style.left = rect.right + 10;
@@ -18,13 +23,14 @@ sentiment.addEventListener('mouseout', e => {
 
 
 
+
 //need to do some weird stuff to get the year at the start of the date for sorting purposes
 //Find a more elegant way of solving this 
 const addCase = (casedata, id) => {
     date = casedata.date.substr(-4,) + '/' + casedata.date.substr(0,casedata.date.length-5)
     let html = `
     <tr>
-        <td>${casedata.citation}</td>
+        <td><a href = '#'>${casedata.citation}</a></td>
         <td>${casedata.percentage}</td>
         <td>${casedata.location}</td>
         <td>${casedata.age}</td>
@@ -37,18 +43,26 @@ const addCase = (casedata, id) => {
     table.innerHTML += html;
 }
 
-
 //Gets the data base, and adds each document one row at a time
 //Maybe connect this to the page load somehow, rather than just floating in the middle of nowhere
-db.collection('casebank').get().then((snapshot) => {
-    snapshot.docs.forEach(doc => {
-        addCase(doc.data(), doc.id);
-    });
-}).catch(err => {
-    console.log(err);
-});
+
+function fetchCases(){
+    if (fetchstatus === false){
+        fetchstatus = true;
+        results.style.display = "";
+        dashboard.style.display = "";
+        db.collection('casebank').get().then((snapshot) => {
+            snapshot.docs.forEach(doc => {
+                addCase(doc.data(), doc.id);
+            });
+        }).catch(err => {
+            console.log(err);
+        });
+    };
+}
 
 function myFunction() {
+    fetchCases();
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("searchbar");
     filter = input.value.toUpperCase();
@@ -68,11 +82,12 @@ function myFunction() {
     }
 
 function filter(){
-    filtersalary()
-    filteryear()
+    filtersalary();
+    filteryear();
 }
 
 function filtersalary(){
+    fetchCases();
     let input, filter, table, tr, td, i, txtValue; 
     input = document.getElementById('salary');
     pattern = /\$?([0-9]{1,2},?[0-9]{1,3})/
@@ -117,6 +132,7 @@ function filteryear(){
 }
 
 function highlight(){
+    fetchCases()
     let table = document.getElementById('resultstable');
     let tr = table.getElementsByTagName('tr');
     for (i = 1; i < tr.length; i++){
@@ -131,7 +147,8 @@ function highlight(){
 
 function calculator(){
     let calc = document.getElementById('calculator');
-    let shield = document.getElementById('shield')
+    let shield = document.getElementById('shield');
+    let explainer = document.querySelector('#calcexplainer');
     if (calc.style.display == "none") {
         calc.style.display = ''
         shield.style.display = ''
@@ -139,7 +156,22 @@ function calculator(){
         calc.style.display = 'none'
         shield.style.display = 'none'
     }
+
+
+    //Random background gradient colour generator
+    function genrand(){
+        let r = Math.min(Math.round(Math.random()*255), 140);
+        let g = Math.min(Math.round(Math.random()*255), 140);
+        let b = Math.min(Math.round(Math.random()*255), 140);
+        return `rgba(${r},${g},${b},1)`
+    }
+    
+    let gradient = `linear-gradient(132deg, ${genrand()} 0%, ${genrand()} 100%, ${genrand()} 100%)`;
+    explainer.style.background = gradient;
 }
+
+
+
 
 function calculate(){
     let inputs = document.getElementsByClassName('calcinput')
@@ -163,7 +195,7 @@ function calculate(){
     let periodical_compensation = 0.8*(salary - temp_salary) * (sickleave/30);
     let total = Math.round(incapacity_compensation + medical_compensation + periodical_compensation);
     console.log(incapacity_compensation, medical_compensation, periodical_compensation)
-    output.innerHTML = '<span>Projected Compensation:</span> $' + String(total).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    output.innerHTML = '<span>Predicted Compensation:</span> $' + String(total).replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
 function sort(n){
